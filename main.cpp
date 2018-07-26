723b54dc6b28703b7ca5cd9ceffadd1eec3090a4
Unknown <geraldcdx5@gmail.com>
2018-07-26 17:41:46 +0800

    added splash screen

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <QtWidgets>
 #include "mainwindow.h"
 #include "lapi.h"
-
+#include <QSplashScreen>
+#include <QTimer>
 int main(int argc, char *argv[])
 {
+
     QApplication a(argc, argv);
     a.setOrganizationName("YYJHAO");
     a.setApplicationName("IVLE Downloader");
 
+    QSplashScreen *splashed=new QSplashScreen;
+    splashed->setPixmap(QPixmap(":/icons/transparentlogo.png"));
+    splashed->show();
+    QTimer::singleShot(2000,splashed,SLOT(close()));
     MainWindow w;
+
     // w.show();
     return a.exec();
 }
